2a0a75a9e187c2b6b67a64ecb75a195fc2816411
ta-king <ta-king@users.noreply.github.com>
2017-03-01 20:15:44 +0200

    Adding Categories crud part 1.

---
+++ backend/views/category/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use abcms\shop\models\Category;
+
+/* @var $this yii\web\View */
+/* @var $searchModel abcms\shop\models\CategorySearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Categories';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="category-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <p>
+        <?= Html::a('Create Category', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+    <?=
+    GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+            'id',
+            'name',
+            [
+                'attribute' => 'parentId',
+                'value' => function($model){
+                    return $model->parentName;
+                },
+                'filter' => Category::getParentsList(),
+            ],
+            ['class' => 'abcms\library\grid\ActivateColumn'],
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]);
+    ?>
+</div>
---
+++ models/Category.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace abcms\shop\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "shop_category".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property integer $parentId
+ * @property integer $active
+ * @property integer $deleted
+ */
+class Category extends \abcms\library\base\BackendActiveRecord
+{
+
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'shop_category';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['parentId', 'active'], 'integer'],
+            [['name'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            [
+                'class' => \abcms\multilanguage\behaviors\ModelBehavior::className(),
+                'attributes' => [
+                    'name',
+                ],
+            ],
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'parentId' => 'Parent',
+            'active' => 'Active',
+            'deleted' => 'Deleted',
+        ];
+    }
+
+    /**
+     * Get Category model that this category belongs to
+     * @return mixed
+     */
+    public function getParent()
+    {
+        return $this->hasOne(Category::className(), ['id' => 'parentId']);
+    }
+    
+    /**
+     * Get parent category name
+     * @return string|null
+     */
+    public function getParentName()
+    {
+        return $this->parent ? $this->parent->name : null;
+    }
+    
+    /**
+     * Return parents list array, to be used in drop down lists.
+     * @return array
+     */
+    public static function getParentsList($excludeId = null)
+    {
+        $query = self::find()->andWhere(['parentId' => null])->orderBy('name ASC');
+        if($excludeId){
+            $query->andWhere(['not in', 'id', $excludeId]);
+        }
+        $models = $query->all();
+        return ArrayHelper::map($models, 'id', 'name');
+    }
+
+}
